60485059692ba9be5d756f28795f6f195529f2b6
Mohamed Alameen <malameen085@gmail.com>
2020-09-26 17:43:19 +0200

    Implement CQRS and add create order command and handler

---
+++ Application/Orders/Commands/CreateOrderCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Orders.Commands
+{
+    public class CreateOrderCommand : IRequest<int>
+    {
+        public int NumberOfItems { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Application/Orders/Handlers/CreateOrdersCommadHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Application.Orders.Commands;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Orders.Handlers
+{
+    class CreateOrdersCommadHandler : IRequestHandler<CreateOrderCommand, int>
+    {
+        public IApplicationDbContext _context { get; }
+
+        public CreateOrdersCommadHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = new Order()
+            {
+                NumberOfItems = request.NumberOfItems,
+                CustomerId = request.CustomerId
+            };
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return order.Id;
+        }
+    }
+}
