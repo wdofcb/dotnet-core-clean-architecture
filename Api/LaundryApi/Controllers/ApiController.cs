3babdd20b1b5f6af238a7920a184aadfc01ea897
Mohamed Alameen <malameen085@gmail.com>
2020-09-26 18:23:51 +0200

    Add orders controller

--- Api/LaundryApi/Controllers/ApiController.cs
+++ Api/LaundryApi/Controllers/ApiController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace LaundryApi.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public abstract class ApiController : Controller
     {
         private IMediator _mediator;
---
+++ Api/LaundryApi/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Orders.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LaundryApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class OrdersController : ApiController
+    {
+        public async Task<ActionResult<int>> Create(CreateOrderCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+    }
+}
