998031737d22688a2cafc412066dc563151b35d7
Mohamed Alameen <malameen085@gmail.com>
2020-09-26 17:57:12 +0200

    Add ValidationBehavior to validate our commands

---
+++ Application/Orders/Validators/CreateOrderCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Orders.Commands;
+using FluentValidation;
+
+namespace Application.Orders.Validators
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    { 
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(v => v.NumberOfItems).NotNull();
+            RuleFor(v => v.CustomerId).NotNull();
+        }
+    }
+}
