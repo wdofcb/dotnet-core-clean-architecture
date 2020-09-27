d1ee614ae5531cee86ae3954ef060bfe1f34a90b
Mohamed Alameen <malameen085@gmail.com>
2020-09-27 09:11:34 +0200

    use MediatR to get customer orders

--- Api/LaundryApi/Controllers/OrdersController.cs
+++ Api/LaundryApi/Controllers/OrdersController.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Orders.Commands;
+using Application.Orders.Queries;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace LaundryApi.Controllers
@@ -8,10 +11,16 @@ namespace LaundryApi.Controllers
     [Route("api/[controller]")]
     public class OrdersController : ApiController
     {
+        [HttpPost]
         public async Task<ActionResult<int>> Create(CreateOrderCommand command)
         {
             return await Mediator.Send(command);
         }
 
+        [HttpGet]
+        public async Task<IList<Order>> Get()
+        {
+            return await Mediator.Send(new GetCustomerOrdersQuery() { CustomerId = 3 });
+        }
     }
 }
